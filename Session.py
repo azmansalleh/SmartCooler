20a104443a165e85e8c72513896a2a49e130bdbe
Azman <32592083+azmansalleh@users.noreply.github.com>
2018-06-13 10:46:46 +0800

    Update Session.py

--- Session.py
+++ Session.py
@@ -1,12 +1,19 @@
 class Session():
 	def __init__(self):
+		self.reset()
+		
+	def reset(self):
 		self.window = "Smart Cooler"
 		self.img = "frame.jpg"
 		self.url = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0"
 		self.key = "f2378de47ace413bbf4dd2538745c40b"
 		self.groupID = "liquidstudiosg"
-		self.verification = False
 
+		self.nameCounter = 0
+		self.verification = False
+		self.switch = True
+		self.verificationTime = 0
+		
 	def startSession(self,CF):
 		CF.BaseUrl.set(self.url)
 		CF.Key.set(self.key)
@@ -18,11 +25,27 @@ class Session():
 	def getGroup(self):
 		return self.groupID
 
+	def welcomeName(self,time,PG,name):
+		if self.nameCounter == 0:
+			self.nameSound = PG.mixer.Sound("audio/"+name+".wav")
+			self.restockSound = PG.mixer.Sound("audio/test.wav")
+			self.nameSound.play()
+			time.sleep(3.5)
+			self.restockSound.play()
+			self.nameCounter +=1
+
 	def verified(self):
 		self.verification = True
+		self.verificationTime += 1	
 
 	def checkVerification(self):
 		return self.verification
+
+	def clearVerification(self):
+		self.verification = False
+
+	def getCounter(self):
+		return self.nameCounter
 		
 
 
