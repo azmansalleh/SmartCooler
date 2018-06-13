7ee9d20bb1132835321e839d01445e3e2012ddaa
Azman <32592083+azmansalleh@users.noreply.github.com>
2018-06-13 10:46:25 +0800

    Create Switch.py

---
+++ Switch.py
@@ -0,0 +1,29 @@
+#import serial
+
+class Switch():
+	def __init__(self,serial):
+		self.reset(serial)
+		
+	def reset(self,serial):
+		self.port = "COM5"
+		self.rate = 9600
+		self.arduino = serial.Serial(self.port, self.rate, timeout=.1)	
+		self.door = "Closed"
+
+	def read(self):
+		while 1:
+			self.data = self.arduino.readline()[:-2].decode('utf-8')
+			return self.data
+
+	def openDoor(self):
+		self.door = "Opened"
+
+	def closeDoor(self):
+		self.door = "Closed"
+
+	def checkDoor(self):
+		return self.door
+		
+
+# #test.printme()
+# #print (test.testme())
